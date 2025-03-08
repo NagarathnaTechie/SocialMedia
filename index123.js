e43ed60a5bb6291ce2e2d06cc8d5991105e88135
Nagarathna S <166543180+NagarathnaTechie@users.noreply.github.com>
2025-03-08 23:48:36 +0530

    Update index123.js

--- index123.js
+++ index123.js
@@ -18,7 +18,7 @@ const Bg2 = document.querySelector('.bg-2');
 const Bg3 = document.querySelector('.bg-3');
 
 
-// ============== SIDEBAR ============== 
+// ============== SIDEBAR ==============++
 
 // Remove active class from all menu items
 const changeActiveItem = () => {
